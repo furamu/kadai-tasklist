9fad8de20927b262b6afdc03a3816fc1657890a7
furamu <ganbabenkyo@yahoo.co.jp>
2021-09-16 10:49:38 +0000

    saiteisyutu1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
      else 
        flash.now[:danger] = "Taskは更新されませんでした"
        render:edit
-      end
+     end
   end
 
   def destroy
@@ -46,7 +46,10 @@ class TasksController < ApplicationController
     
     flash[:success] = "Taskは正常に削除されました"
     redirect_to tasks_url
-  end
+  end  
+  
+  
+  private
   
   def task_params
     params.require(:task).permit(:content)
