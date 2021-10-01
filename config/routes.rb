46973829ddb14a9ba20226811cd4014be65aad8e
furamu <ganbabenkyo@yahoo.co.jp>
2021-10-01 04:11:03 +0000

    lesson20_kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :tasks, only: [:create, :destroy]
+
 end
---
+++ db/migrate/20210930020149_rename_neme_column_to_users.rb
@@ -0,0 +1,6 @@
+class RenameNemeColumnToUsers < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :users, :neme, :name
+
+  end
+end
