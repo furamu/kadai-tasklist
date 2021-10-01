30c3a1e7d55b9b3664b9d55cf7a06806e2b07325
furamu <ganbabenkyo@yahoo.co.jp>
2021-10-01 12:52:32 +0000

    saiteisyutu_3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
   root to: 'tasks#index'
   resources :tasks
   
@@ -15,6 +10,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :tasks, only: [:create, :destroy]
+  
 
 end
